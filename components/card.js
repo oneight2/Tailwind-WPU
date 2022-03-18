630232d6e76a4bab9701995c28a086a2bf04e7b5
oneight2 <syarif.cibatu@gmail.com>
2022-03-18 21:08:30 +0700

    card

---
+++ components/card.js
@@ -0,0 +1,13 @@
+export default function card() {
+  return (
+    <a
+      href="https://nextjs.org/docs"
+      className="m-4 p-6 border-[1px] max-w-[45%] border-gray-200 rounded-xl hover:border-blue-600 hover:text-blue-600"
+    >
+      <h2 className="text-2xl mb-4">Documentation &rarr;</h2>
+      <p className="text-xl">
+        Find in-depth information about Next.js features and API.
+      </p>
+    </a>
+  );
+}
